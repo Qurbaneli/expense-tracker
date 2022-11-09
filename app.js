235000f007f08816dc5a720a9d1707d6526e3aac
Qurbaneli Veliyev <74726434+Qurbaneli@users.noreply.github.com>
2022-11-09 08:23:20 +0400

    Upload files

---
+++ app.js
@@ -0,0 +1,73 @@
+let form=document.querySelector("#form")
+let text=document.querySelector("#text")
+let amount=document.querySelector("#amount")
+let income=document.querySelector("#money-plus")
+let expense=document.querySelector("#money-minus")
+let balance=document.querySelector("#balance")
+let list=document.querySelector("#list")
+
+form.addEventListener("submit",function(e){
+    e.preventDefault()
+
+    if(amount.value.trim().length >0 && text.value.trim().length >0)
+    {
+        if(amount.value!=0)
+        {
+            
+            
+
+            if(amount.value>0)
+            {
+                list.innerHTML+=`<li class="plus">
+                ${text.value} <span>+$ ${amount.value}</span><button class="delete-btn">x</button>
+            </li>`
+            let incomeAmount=+income.innerHTML.split(" ")[0]
+            income.innerHTML= incomeAmount + +amount.value+" $"
+            }
+            else
+            {
+                list.innerHTML+=`<li class="minus">
+                ${text.value}  <span>-$ ${amount.value}</span><button class="delete-btn">x</button>
+            </li>`
+            let expenseAmount=+expense.innerHTML.split(" ")[0]
+            expense.innerHTML= expenseAmount+ +amount.value+" $"
+            }
+
+            balance.innerHTML= +income.innerHTML.split(" ")[0] + +expense.innerHTML.split(" ")[0] +" $"
+            amount.value=""
+            text.value=""
+        }
+        else
+        {
+            alert("The amount should not equal to zero")
+        }
+
+    }
+
+    else
+    {
+        alert("You should add the fields")
+    }
+
+})
+
+list.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("delete-btn"))
+    {
+        let deletedAmount=+e.target.previousElementSibling.innerHTML.split(" ")[1];
+        
+        if(deletedAmount>0)
+        {
+            let incomeAmount=+income.innerHTML.split(" ")[0]
+            income.innerHTML= incomeAmount- +deletedAmount + " $"
+        }
+        else
+        {
+            let expenseAmount=+expense.innerHTML.split(" ")[0]
+            expense.innerHTML= expenseAmount - +deletedAmount + " $"
+        }
+       
+        balance.innerHTML= +income.innerHTML.split(" ")[0] + +expense.innerHTML.split(" ")[0] +" $"
+        e.target.parentElement.remove()
+    }
+})
